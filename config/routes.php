8ae7241dea93ef0a089990f91d71ccabc26860f2
Davidson Ratis <davidsonmakarios@gmail.com>
2022-12-28 19:59:52 -0300

    configured registration route

--- config/routes.php
+++ config/routes.php
@@ -4,11 +4,13 @@ $routes = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
 use App\Controller\ {
   AuthController,
   HomeController,
+  RegisterController,
   NotFoundController
 };
 
 echo $match = match ($routes) {
   '/' => HomeController::index(),
   '/auth' => AuthController::index(),
+  '/auth/register' => RegisterController::index(),
   default => NotFoundController::index()
 };
