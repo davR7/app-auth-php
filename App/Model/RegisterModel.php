808b2cec90c2d7c6bad064122a14f016f9b53e5c
Davidson Ratis <davidsonmakarios@gmail.com>
2022-12-28 15:48:46 -0300

    settings - RegisterModel

---
+++ App/Model/RegisterModel.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Model;
+
+use App\Dao\UserDao;
+use app\Helpers\EmptyInput;
+
+class RegisterModel {
+  public $fullname, $email, $password, $passwordConfirm;
+
+  public function save()
+  {
+    $register = new UserDao();
+
+    if (EmptyInput::test($this)) {
+      $err = ['ok' => false, 'message' => EmptyInput::info($this)];
+      echo json_encode($err);
+      exit();
+    }
+
+    if (!filter_var($this->email, FILTER_VALIDATE_EMAIL)) {
+      $res = ['ok' => false, 'message' => 'Email inválido.'];
+      echo json_encode($res);
+      exit();
+    }
+
+    if ($register->selectOne(['email' => $this->email])) {
+      $err = ['ok' => false, 'message' => 'Usuário já cadastrado.'];
+      echo json_encode($err);
+      exit();
+    }
+
+    if ($this->password !== $this->passwordConfirm) {
+      $err = ['ok' => false, 'message' => 'As senhas não são iguais.'];
+      echo json_encode($err);
+      exit();
+    }
+
+    $this->password = password_hash($this->password, PASSWORD_BCRYPT);
+    $register->insert($this);
+    $success = ['ok' => true, 'message' => 'Usuário Cadastrado com Sucesso!'];
+    echo json_encode($success);
+  }
+}
