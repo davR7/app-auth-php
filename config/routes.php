be6a878ae04645353d6eff075033851c550be1aa
Davidson Ratis <davidsonmakarios@gmail.com>
2023-01-07 11:39:34 -0300

    updated - login and logout route

--- config/routes.php
+++ config/routes.php
@@ -4,6 +4,8 @@ $routes = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
 use App\Controller\ {
   AuthController,
   HomeController,
+  LoginController,
+  LogoutController,
   RegisterController,
   NotFoundController
 };
@@ -11,6 +13,8 @@ use App\Controller\ {
 echo $match = match ($routes) {
   '/' => HomeController::index(),
   '/auth' => AuthController::index(),
+  '/auth/login' => LoginController::index(),
   '/auth/register' => RegisterController::index(),
+  '/logout' => LogoutController::index(),
   default => NotFoundController::index()
 };
