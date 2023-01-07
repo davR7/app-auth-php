2096544700bcef6ba6fb79f5b59fcfcf7950f163
Davidson Ratis <davidsonmakarios@gmail.com>
2023-01-07 10:57:40 -0300

    created - home page

--- App/View/Main/Home.php
+++ App/View/Main/Home.php
@@ -1,1 +1,6 @@
 <h2 class="my-home">Home</h2>
+<div class="info-user flex-column-x">
+  <p class="info-user__name">Nome: <?= $_SESSION['fullname']; ?></p>
+  <p class="info-user__email">Email: <?= $_SESSION['email']; ?></p>
+  <a class="info-user__link" href="/logout">Sair</a>
+</div>
