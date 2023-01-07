c5cfb90113a57d602eb6bd8776757187a354d000
Davidson Ratis <davidsonmakarios@gmail.com>
2023-01-07 11:04:45 -0300

    updated - validation method

--- App/Controller/Controller.php
+++ App/Controller/Controller.php
@@ -4,11 +4,13 @@ namespace App\Controller;
 
 use App\Model\Model;
 use App\Helpers\{
+  Auth,
   FileBuildPath,
   Mustache
 };
 
-abstract class Controller {
+abstract class Controller
+{
   /**
    * render: directory path based on operating system
    * @access public
@@ -18,16 +20,33 @@ abstract class Controller {
    */
   public static function render($filename, ?Model $model = null)
   {
+    //page authorization
+    self::validate($filename);
+
     //render header
     echo Mustache::replace('App\\View\\Template\\Header.php', 'style', $filename);
-    
+
     //render content
-    require_once(FileBuildPath::create("App\\View\\Main\\$filename".'.php'));
+    require_once(FileBuildPath::create("App\\View\\Main\\$filename" . '.php'));
 
     //render libraries
     require_once(FileBuildPath::create('App\\View\\Dependencies\\Libraries.php'));
 
     //render footer
     echo Mustache::replace('App\\View\\Template\\Footer.php', 'script', $filename);
+  }
+  public static function validate($filename)
+  /**
+   * validate: checks if the user has permission to access the content of the page.
+   * @access public
+   * @param string $filename
+   * @return void
+   */
+  {
+    if ($filename !== 'Auth') {
+      Auth::checkUnauthenticated();
+    } else {
+      Auth::checkAuthenticated(); 
+    }
   }
 }
